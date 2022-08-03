f70870206a68ebb2964df08c045368ca83bd8911
keigrey <keixgrey@gmail.com>
2022-08-03 22:04:01 +0900

    feat: Add recipe_review table

---
+++ server/db/knex/migrations/20220803122116_recipe_review.js
@@ -0,0 +1,38 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("recipe_review", function (table) {
+    table
+      .integer("account_id")
+      .notNullable()
+      .references("id")
+      .inTable("account")
+      .onDelete("CASCADE");
+    table
+      .integer("family_id")
+      .notNullable()
+      .references("id")
+      .inTable("family")
+      .onDelete("CASCADE");
+    table
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipe")
+      .onDelete("CASCADE");
+    table.date("date").notNullable();
+    table.integer("review");
+
+    table.primary(["account_id", "family_id", "recipe_id"]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("recipe_review");
+};
