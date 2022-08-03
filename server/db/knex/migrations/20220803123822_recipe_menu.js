830f636fec4f19667b876c1424645ce8c9607b32
keigrey <keixgrey@gmail.com>
2022-08-03 22:04:43 +0900

    feat: Add recipe_menu table

---
+++ server/db/knex/migrations/20220803123822_recipe_menu.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("recipe_menu", function (table) {
+    table
+      .integer("account_id")
+      .notNullable()
+      .references("id")
+      .inTable("account")
+      .onDelete("CASCADE");
+    table
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipe")
+      .onDelete("CASCADE");
+    table.boolean("review_request").notNullable();
+    table.date("date").notNullable();
+
+    table.primary(["account_id", "recipe_id"]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("recipe_menu");
+};
