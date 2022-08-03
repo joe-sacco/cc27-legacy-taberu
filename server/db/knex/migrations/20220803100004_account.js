1a3a6b1f70d34f5e48e48924e39c1543fb62dbfd
keigrey <keixgrey@gmail.com>
2022-08-03 22:02:23 +0900

    feat: Add account table

---
+++ server/db/knex/migrations/20220803100004_account.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("account", function (table) {
+    table.increments("id").primary();
+    table.string("first_name", 32).notNullable();
+    table.string("last_name", 32).notNullable();
+    table.string("email", 64).notNullable();
+    table.string("password", 32).notNullable();
+    table.integer("pincode", 4).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("account");
+};
