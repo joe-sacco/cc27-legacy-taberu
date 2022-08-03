be0919289e1adb080e98d1c5e3ba80537dbd627b
keigrey <keixgrey@gmail.com>
2022-08-03 22:03:19 +0900

    feat: Add ingredient table

---
+++ server/db/knex/migrations/20220803115850_ingredient.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("ingredient", function (table) {
+    table.increments("id").primary();
+    table.string("name", 32).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("ingredient");
+};
