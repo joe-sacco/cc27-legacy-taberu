f802f742db53ef08ea4b45cfcefad7d0aaf3160c
keigrey <keixgrey@gmail.com>
2022-08-03 22:04:15 +0900

    feat: Add recipe_memo table

---
+++ server/db/knex/migrations/20220803123429_recipe_memo.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("recipe_memo", function (table) {
+    table
+      .integer("account_id")
+      .notNullable()
+      .references("id")
+      .inTable("account")
+      .onDelete("CASCADE");
+    table
+      .integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipe")
+      .onDelete("CASCADE");
+    table.string("memo", 128);
+
+    table.primary(["account_id", "recipe_id"]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("recipe_memo");
+};
