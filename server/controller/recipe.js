3d65e3920939c62fb7fc375e5576ac6ae0fdd350
Hiro-Kanetake <git@github.com>
2022-08-05 08:02:45 +0900

    feat: Add controller for recipe

--- server/controller/recipe.js
+++ server/controller/recipe.js
@@ -1,15 +1,13 @@
 const accountModel = require("../model/recipe");
-// const dataValidated = require("./validation");
+const dataValidated = require("./validation");
 
 module.exports = {
   async createRecipe(req, res) {
-    console.log("🔴🔴🔴🔴")
-    const recipeInfo = await req.body;
-    await accountModel.createRecipe(recipeInfo);
-    res.status(201).send("SUCCESS MESSAGE");
+    if (validator.validateNewAccount(recipeInfo)) {
+      await accountModel.createAccount(recipeInfo);
+      res.status(201).send("RECIPE ADDED");
+    } else {
+      res.status(406).send("BAD DATA. TRY AGAIN");
+    }
   },
-
-//   async getRecipe(req, res) {
-//     //
-//   },
 };
