c0a6359278434bdb128ddf00a3ab959e681b4dea
Hiro-Kanetake <git@github.com>
2022-08-05 08:03:57 +0900

    feat: Add model for recipe

--- server/model/recipe.js
+++ server/model/recipe.js
@@ -5,13 +5,6 @@ const RECIPE_TABLE = "recipe";
 module.exports = {
   RECIPE_TABLE,
 
-  // selectRecipe() {
-  //   return knex
-  //     .select("*")
-  //     .from(RECIPE_TABLE)
-  //     .then((data) => console.log("MODEL", data));
-  // },
-  // firstName, lastName, email, password, pincode, image
   createRecipe(data) {
     return knex.insert(data).into(RECIPE_TABLE);
   },
