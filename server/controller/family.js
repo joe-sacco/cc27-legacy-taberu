aa345369dcf98a7ddc5b972ddb946fca4559903d
Hiro-Kanetake <git@github.com>
2022-08-05 08:15:30 +0900

    feat: Add controller for family

---
+++ server/controller/family.js
@@ -0,0 +1,14 @@
+const familyModel = require("../model/family");
+const validator = require("./validation");
+
+module.exports = {
+  async createFamily(req, res) {
+    const familyInfo = await req.body;
+    if (validator.validateNewAccount(familyInfo)) {
+      await familyModel.createFamily(familyInfo);
+      res.status(201).send("FAMILY ADDED");
+    } else {
+      res.status(406).send("BAD DATA. TRY AGAIN");
+    }
+  },
+}
