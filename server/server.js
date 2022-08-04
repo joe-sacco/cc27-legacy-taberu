060af7346d177c22957e4cbe4ea31bbeab8ea7f8
keigrey <keixgrey@gmail.com>
2022-08-04 20:07:30 +0900

    feat: Add router for /acccount/ requests to server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const knex = require("./db/knex/knex.js");
+// const knex = require("./db/knex/knex.js");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-const accountRouter = require("./routes/account");
-app.use("/account", accountRouter);
-
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
 });
+
+const accountRouter = require("./routes/account");
+app.use("/account", accountRouter);
