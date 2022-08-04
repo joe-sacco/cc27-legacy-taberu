72bc0037cc8a5c058d3cd8c82dd584a8a1f84567
Hiro-Kanetake <git@github.com>
2022-08-05 08:15:05 +0900

    feat: Add controller for family

--- server/controller/recipe.js
+++ server/controller/recipe.js
@@ -3,8 +3,8 @@ const dataValidated = require("./validation");
 
 module.exports = {
   async createRecipe(req, res) {
-    if (validator.validateNewAccount(recipeInfo)) {
-      await accountModel.createAccount(recipeInfo);
+    if (validator.validateNewRecipe(recipeInfo)) {
+      await accountModel.createRecipe(recipeInfo);
       res.status(201).send("RECIPE ADDED");
     } else {
       res.status(406).send("BAD DATA. TRY AGAIN");
