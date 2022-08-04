f1495bf329a5ec44765381dddcffecea0d873f19
Hiro-Kanetake <git@github.com>
2022-08-04 15:56:15 +0900

    feat: Alter table name to daily_menu

---
+++ server/db/knex/migrations/20220804064903_change_table_name_daily_menu.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.renameTable("recipe_menu", "daily_menu");
+};
+  
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable("daily_menu");
+};
