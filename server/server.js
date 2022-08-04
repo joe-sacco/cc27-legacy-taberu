287b62d3400ccd4391daee37e61dfdbb3337de90
keigrey <keixgrey@gmail.com>
2022-08-04 12:18:41 +0900

    feat: Add account route to server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
-const knex = require("./db/knex/knex.js")
+const knex = require("./db/knex/knex.js");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.status(354).send();
-})
+const accountRouter = require("./routes/account");
+app.use("/account", accountRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`);
-})
+  console.log(`Server started on PORT: ${PORT}`);
+});
