7e2c3e9146241987bd284acfd42414cfe51d0ec2
Hiro-Kanetake <git@github.com>
2022-08-05 08:23:43 +0900

    feat: Add router for family in server.js

--- server/server.js
+++ server/server.js
@@ -17,3 +17,6 @@ app.use("/account", accountRouter);
 
 const recipeRouter = require("./routes/recipe");
 app.use("/recipe", recipeRouter);
+
+const familyRouter = require("./routes/family");
+app.use("/account/:id/family", familyRouter);
