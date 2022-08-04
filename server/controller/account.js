b1cd2877621d5af41197a969d31ec90296b4d6e8
keigrey <keixgrey@gmail.com>
2022-08-04 22:17:10 +0900

    feat: Add 4xx response if database entry is not found

--- server/controller/account.js
+++ server/controller/account.js
@@ -1,22 +1,32 @@
 const accountModel = require("../model/account");
-const dataValidated = require("./validation");
+const validator = require("./validation");
 
 module.exports = {
   async createAccount(req, res) {
     const accountInfo = await req.body;
-    await accountModel.createAccount(accountInfo);
-    res.status(201).send("ACCOUNT CREATED");
+    if (validator.validateNewAccount(accountInfo)) {
+      await accountModel.createAccount(accountInfo);
+      res.status(201).send("ACCOUNT CREATED");
+    } else {
+      res.status(406).send("BAD DATA. TRY AGAIN");
+    }
   },
 
   async authenticateAccount(req, res) {
     const loginInfo = await req.body;
-    const [associatedAccount] = await accountModel.authenticateAccount(
-      loginInfo
-    );
-    if (!associatedAccount) {
-      res.status(404).send("ACCOUNT NOT FOUND");
+
+    if (!validator.validateCredentials(loginInfo)) {
+      res.status(406).send("BAD DATA. TRY AGAIN");
     } else {
-      res.status(201).json({ id: associatedAccount.id });
+      const [associatedAccount] = await accountModel.authenticateAccount(
+        loginInfo
+      );
+
+      if (!associatedAccount) {
+        res.status(404).send("ACCOUNT NOT FOUND");
+      } else {
+        res.status(201).json({ id: associatedAccount.id });
+      }
     }
   },
 };
