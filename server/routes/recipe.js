9c2a055ab3fa8887ed34bf202ed0cc7508c5befa
Hiro-Kanetake <git@github.com>
2022-08-05 12:07:04 +0900

    feat: Add get recipe routes

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const recipeController = require("../controller/recipe");
 
 router.post("/", recipeController.createRecipe);
+router.get("/", recipeController.getRecipe);
 
 module.exports = router;
