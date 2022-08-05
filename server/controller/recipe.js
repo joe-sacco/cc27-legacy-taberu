de601f5079ce6d3a6d8d117bcfe63fcc90fc1598
Hiro-Kanetake <git@github.com>
2022-08-05 11:18:36 +0900

    fix: Add variable for recipeInfo

--- server/controller/recipe.js
+++ server/controller/recipe.js
@@ -1,10 +1,11 @@
-const accountModel = require("../model/recipe");
+const recipeModel = require("../model/recipe");
 const validator = require("./validation");
 
 module.exports = {
   async createRecipe(req, res) {
+    const recipeInfo = await req.body;
     if (validator.validateNewRecipe(recipeInfo)) {
-      await accountModel.createRecipe(recipeInfo);
+      await recipeModel.createRecipe(recipeInfo);
       res.status(201).send("RECIPE ADDED");
     } else {
       res.status(406).send("BAD DATA. TRY AGAIN");
