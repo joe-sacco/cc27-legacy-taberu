3b65cf6c0a1bdf056fe0cbf8678348851b757510
Hiro-Kanetake <git@github.com>
2022-08-05 11:31:01 +0900

    fix: Edit drop table name

--- server/db/knex/migrations/20220803123822_recipe_menu.js
+++ server/db/knex/migrations/20220803123822_recipe_menu.js
@@ -28,5 +28,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable("daily_menu");
+  return knex.schema.dropTable("recipe_menu");
 };
