41145485580e3f3a0db946724f6915fcc65e6686
Hiro-Kanetake <git@github.com>
2022-08-05 11:22:45 +0900

    fix: Remove id

--- server/db/knex/seeds/03-create_recipe.js
+++ server/db/knex/seeds/03-create_recipe.js
@@ -4,14 +4,14 @@
  */
  exports.seed = async function(knex) {
   await knex('recipe').insert([
-    {id: 1, name: 'Curry'},
-    {id: 2, name: 'Rice'},
-    {id: 3, name: 'Sunny Side Up Egg'},
-    {id: 4, name: 'Egg Omlette'},
-    {id: 5, name: 'Cream Stew'},
-    {id: 6, name: 'Stir Fried Mix Vegetable'},
-    {id: 7, name: 'Meat Sauce Spagetthi'},
-    {id: 8, name: 'Fried Rice'},
-    {id: 9, name: 'Hamburg'},
+    {name: 'Curry'},
+    {name: 'Rice'},
+    {name: 'Sunny Side Up Egg'},
+    {name: 'Egg Omlette'},
+    {name: 'Cream Stew'},
+    {name: 'Stir Fried Mix Vegetable'},
+    {name: 'Meat Sauce Spagetthi'},
+    {name: 'Fried Rice'},
+    {name: 'Hamburg'},
   ]);
 };
