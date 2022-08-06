e8a742fa99050494b31e2035779c6051bcebfc25
Hiro-Kanetake <git@github.com>
2022-08-06 17:15:56 +0900

    feat: Implement adding family member

--- client/src/OwnerFamily.tsx
+++ client/src/OwnerFamily.tsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios"
+import axios from "axios";
 import './App.css';
+import { useForm } from "react-hook-form";
 
 const DB_URL = "http://localhost:8080"
 // Hardcoded ownerAccount ID
 const obtainedId = 2;
 
+interface addFamily {
+  lastname: string;
+  firstname: string;
+}
+
 //TO DO::
 //[ ] Implement the DB_URL to switch automatically between localhost and Heroku depending on production/development
 //[ ] Implement a way to catcht the accountOwner's ID
-//[ ] Implement add family function
+//[✅] Implement add family function
+//[ ] Make the page show newly added family automatically
 
 const OwnerFamily: React.FC = () => {
   const [familyMember, setFamilyMember] = useState<{id: number, account_id: number, first_name: string, last_name: string}[]>([],);
+  const [newFamily, setNewFamily] = useState<{account_id: number, first_name: string, last_name: string}>();
   useEffect(() => {
     const accountId = {
       params: {
@@ -32,16 +40,43 @@ const OwnerFamily: React.FC = () => {
       });
   }, [familyMember]);
 
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm<addFamily>({
+    defaultValues: {
+      lastname: "",
+      firstname: "",
+    },
+  });
+
+  const newFamilyInfo = {
+    account_id: obtainedId,
+    first_name: "",
+    last_name: "",
+  }
+  const onSubmit = (data: any) => {
+    newFamilyInfo.first_name = data.firstname;
+    newFamilyInfo.last_name = data.lastname;
+    console.log("🔴🔴🔴🔴🔴", familyMember)
+    setNewFamily(newFamilyInfo)
+  };
+  useEffect(() => {
+    if (newFamily) axios.post(`${DB_URL}/account/${obtainedId}/family`, newFamily);
+  }, [newFamily])
+
   return (
     <div className="OwnerRegist">
       <h1>taberu</h1>
       <main>
         <h1 className='logo'>Register</h1>
-        <form action="/OwnerLoginMain">
+        {/* <form action="/OwnerLoginMain"> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="lastname">Last Name *</label>
-          <input type="text" name="name" id="lastname" required />
+          <input type="text" id="lastname" {...register("lastname", { required: "this is required"})} />
           <label htmlFor="firstname">First Name *</label>
-          <input type="text" name="name" id="firstname" required />
+          <input type="text" id="firstname" {...register("firstname", { required: "this is required"})} />
           <button>Add</button>
         </form>
       </main>
