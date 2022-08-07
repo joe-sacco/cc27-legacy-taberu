a8e80844e1aef944e52aad2a5e20eaeb8f129d5d
kugaitomomi <tottomo7@gmail.com>
2022-08-07 20:30:39 +0900

    fix: design css about ChildMain

--- client/src/ChildMain.tsx
+++ client/src/ChildMain.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import './components/images.css';
-import dummy from './images/dummy.png';
-import like from './images/like.png';
-import dislike from './images/dislike.png';
+import React, { useState, useEffect } from "react";
+import "./components/images.css";
+import dummy from "./images/dummy.png";
+import like from "./images/like.png";
+import dislike from "./images/dislike.png";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 // const DB_URL = "https://taberu-server.herokuapp.com" || "http://localhost:8080";
 const DB_URL = "http://localhost:8080";
@@ -14,100 +14,110 @@ const DB_URL = "http://localhost:8080";
 
 const ChildMain: React.FC = () => {
   const navigate = useNavigate();
-  const [review, setReview] = useState<{recipe_id: number, review: number}>(); // review: -1 = dislike, 1 = like
-  const [recipeToReview, setRecipeToReview] = useState<{name: string, recipe_id: number}[]>([]);
+  const [review, setReview] = useState<{ recipe_id: number; review: number }>(); // review: -1 = dislike, 1 = like
+  const [recipeToReview, setRecipeToReview] = useState<
+    { name: string; recipe_id: number }[]
+  >([]);
 
   // GET all the recipe that needs to be reviewed
   useEffect(() => {
     const obtainedAccountId = Number(localStorage.getItem("account_id"));
-    const obtainedFamilyId = localStorage.getItem("family_id")
+    const obtainedFamilyId = localStorage.getItem("family_id");
 
     const accountId = {
       params: {
         account_id: obtainedAccountId,
       },
     };
-    if (recipeToReview) axios
-    .get(`${DB_URL}/recipe/requestReview`, accountId)
-    .then((res) => {
-      if (res.data.length > recipeToReview.length) {
-        let lastIndex = recipeToReview.length;
-        setRecipeToReview((prevRecipe) => [...prevRecipe, res.data[lastIndex]])
-      }
-    }).catch((error) => {
-      console.log(error.response.data);
-    });
-  }, [recipeToReview])
+    if (recipeToReview)
+      axios
+        .get(`${DB_URL}/recipe/requestReview`, accountId)
+        .then((res) => {
+          if (res.data.length > recipeToReview.length) {
+            let lastIndex = recipeToReview.length;
+            setRecipeToReview((prevRecipe) => [
+              ...prevRecipe,
+              res.data[lastIndex],
+            ]);
+          }
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+  }, [recipeToReview]);
 
   //POST the review to the recipe_review
   const reviewObj = {
     account_id: Number(localStorage.getItem("account_id")),
     family_id: Number(localStorage.getItem("family_id")),
     recipe_id: review?.recipe_id,
-    review: review?.review
-  }
+    review: review?.review,
+  };
   const requestReview = {
     account_id: Number(localStorage.getItem("account_id")),
     recipe_id: review?.recipe_id,
-    request_review: false
-  }
+    request_review: false,
+  };
   useEffect(() => {
     // if (review) axios
     // .post(`${DB_URL}/recipe/recipeReview`, reviewObj)
     // (2) need to navigate to ChildDone page
     // axios.put(`${DB_URL}/recipe/requestReview`, requestReview)
-    
+
     if (review) {
-      navigate("/ChildDone", {state:{review}});
+      navigate("/ChildDone", { state: { review } });
       console.log("🟡🟡🟡🟡", reviewObj);
     }
-  },[review])
+  }, [review]);
 
   return (
     <div className="TopChild">
       <main>
-        <h1>Did you enjoy the meal?</h1>
-        <div className='mainVisual'>
-          <img src= { dummy } alt="taberu" />
+        <h2>Did you enjoy the meal?</h2>
+        <div className="mainVisual">
+          <img src={dummy} alt="taberu" />
         </div>
-        <div>
-          <p>Please Review Below Recipe</p>
-        </div>
+        <p className="text_chMain">Please Review Below Recipe</p>
         {recipeToReview.map((recipe, i) => {
           return (
-            <div key={recipe.recipe_id + "_" + i}>
+            <div className="reviewArea_chMain" key={recipe.recipe_id + "_" + i}>
               <p>{recipe.name}</p>
               <form>
-              <label> 
-                <img src={ like } alt="like" height={50} />
-                <button type="submit" onClick={(e) => {
-                  e.preventDefault();
-                  setReview({
-                    recipe_id: recipe.recipe_id,
-                    review: 1
-                  });
-                  recipeToReview.splice(i, 1);
-                  }} />
-              </label>
-              <label> 
-                <img src={ dislike } alt="dislike" height={50} />
-                <button type="submit" onClick={(e) => {
-                  e.preventDefault();
-                  setReview({
-                    recipe_id: recipe.recipe_id,
-                    review: -1
-                  });
-                  recipeToReview.splice(i, 1);
-                  }}  />
-              </label>
+                <label>
+                  <img src={like} alt="like" height={100} />
+                  <button
+                    type="submit"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setReview({
+                        recipe_id: recipe.recipe_id,
+                        review: 1,
+                      });
+                      recipeToReview.splice(i, 1);
+                    }}
+                  />
+                </label>
+                <label>
+                  <img src={dislike} alt="dislike" height={100} />
+                  <button
+                    type="submit"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setReview({
+                        recipe_id: recipe.recipe_id,
+                        review: -1,
+                      });
+                      recipeToReview.splice(i, 1);
+                    }}
+                  />
+                </label>
               </form>
             </div>
-          )
+          );
         })}
-        
       </main>
     </div>
   );
-}
+};
 
 export default ChildMain;
