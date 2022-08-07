c09118da2ef1af3a15605ee8bf80cd619d43319a
keigrey <keixgrey@gmail.com>
2022-08-07 20:27:37 +0900

    feat: Add route for GET recipeReview

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -8,5 +8,6 @@ router.post("/requestReview", recipeController.createPlanMenu);
 router.get("/requestReview", recipeController.getPlanMenu);
 router.put("/requestReview", recipeController.updatePlanMenu);
 router.post("/recipeReview", recipeController.createRecipeReview);
+router.get("/recipeReview", recipeController.getRecipeReview);
 
 module.exports = router;
