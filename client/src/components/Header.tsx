6f7ec8e2d5eb91f87c6243792e2e85fc3ea44122
Hiro-Kanetake <git@github.com>
2022-08-07 22:34:59 +0900

    fix: Rename and change link for directory and buttons

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,9 +11,10 @@ function Header() {
       <nav className="nav_content">
         <ul className="nav_list">
             <li className="nav_item"><Link to="/OwnerLoginMain">Top</Link></li>
-            <li className="nav_item"><Link to="/OwnerFamily">Family</Link></li>
-            <li className="nav_item"><Link to="/OwnerRecipe">Recipes</Link></li>
-            <li className="nav_item"><Link to="/ChildMain">Menu To Review</Link></li>
+            <li className="nav_item"><Link to="/OwnerFamily">My Family</Link></li>
+            <li className="nav_item"><Link to="/OwnerRecipe">My Recipes</Link></li>
+            <li className="nav_item"><Link to="/AllRecipe">All Recipes</Link></li>
+            <li className="nav_item"><Link to="/ChildMenu">Family Review Page</Link></li>
           </ul>
       </nav>
      <h1 className="logo">taberu</h1>
