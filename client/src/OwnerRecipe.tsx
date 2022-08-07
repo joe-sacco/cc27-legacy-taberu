35b91e080b7f00d8ee8ac657fb3195fc0081448c
kugaitomomi <tottomo7@gmail.com>
2022-08-08 00:14:48 +0900

    refactor: Design css about OwnerRecipe

--- client/src/OwnerRecipe.tsx
+++ client/src/OwnerRecipe.tsx
@@ -8,9 +8,16 @@ import dummy from "./images/dummy.png";
 const DB_URL = "http://localhost:8080";
 
 const OwnerRecipe: React.FC = () => {
-  const [allRecipes, setAllRecipes] = useState<{ account_id: number; family_id: number; recipe_id: number; name: string; date: Date; review: number }[]>(
-    []
-  );
+  const [allRecipes, setAllRecipes] = useState<
+    {
+      account_id: number;
+      family_id: number;
+      recipe_id: number;
+      name: string;
+      date: Date;
+      review: number;
+    }[]
+  >([]);
 
   const accountId = {
     params: {
@@ -19,12 +26,12 @@ const OwnerRecipe: React.FC = () => {
   };
   useEffect(() => {
     axios
-    .get(`${DB_URL}/recipe/recipeReview`, accountId)
-    .then((res) => {
-      if (res.data.length > allRecipes.length) {
-        let lastIndex = allRecipes.length;
-        setAllRecipes((prevRecipe) => [...prevRecipe, res.data[lastIndex]]);
-      }
+      .get(`${DB_URL}/recipe/recipeReview`, accountId)
+      .then((res) => {
+        if (res.data.length > allRecipes.length) {
+          let lastIndex = allRecipes.length;
+          setAllRecipes((prevRecipe) => [...prevRecipe, res.data[lastIndex]]);
+        }
       })
       .catch((error) => {
         console.log(error.response.data);
@@ -35,37 +42,45 @@ const OwnerRecipe: React.FC = () => {
     <div className="OwnerRecipe">
       <main>
         <img src={dummy} alt="" />
-        <div className="formArea_owRecipe">
-        </div>
+        <div className="formArea_owRecipe"></div>
         <div className="textArea_owRecipe">
           <p>All Recipes Out There</p>
         </div>
-
-        {allRecipes.reverse().map((recipe) => {
-          if (recipe.review > 0) {
-            return (
-              <div key={recipe.recipe_id + "_" + recipe.name} className="reviewArea_owRecipe">
-                <p>{recipe.name}</p>
-                  <p>{"⭐️".repeat(recipe.review)}</p>
-                  {" "}
-              </div>
-            );
-          } else if (recipe.review < 0){
-            return (
-              <div key={recipe.recipe_id + "_" + recipe.name} className="reviewArea_owRecipe">
-                <p>{recipe.name}</p>
+        <div className="reviewArea_owRecipe">
+          {allRecipes.reverse().map((recipe) => {
+            if (recipe.review > 0) {
+              return (
+                <div
+                  key={recipe.recipe_id + "_" + recipe.name}
+                  className="reviewAreaIn_owRecipe"
+                >
+                  <p>{recipe.name}</p>
+                  <p>{"⭐️".repeat(recipe.review)}</p>{" "}
+                </div>
+              );
+            } else if (recipe.review < 0) {
+              return (
+                <div
+                  key={recipe.recipe_id + "_" + recipe.name}
+                  className="reviewAreaIn_owRecipe"
+                >
+                  <p>{recipe.name}</p>
 
                   <p>{"👿".repeat(recipe.review * -1)}</p>
-              </div>
-            );
-          } else {
-            return (
-              <div key={recipe.recipe_id + "_" + recipe.name} className="reviewArea_owRecipe">
-                <p>{recipe.name}</p>
-              </div>
-            );
-          }
-        })}
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  key={recipe.recipe_id + "_" + recipe.name}
+                  className="reviewArea_owRecipe"
+                >
+                  <p>{recipe.name}</p>
+                </div>
+              );
+            }
+          })}
+        </div>
       </main>
     </div>
   );
